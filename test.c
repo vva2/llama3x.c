e6665b93e4e67fab1069808428121700603f2100
vva <vishaladepu007@gmail.com>
2024-12-04 01:19:47 -0600

    added export.slurm file

--- test.c
+++ test.c
@@ -9,17 +9,17 @@
 int main() {
     TrieNode *root = create_trie();
 
-    for(int i = 0; i < 10000000; i++) {
-        insert(root, "abcd");
-        insert(root, "vishal");
-        insert(root, "vivek");
-        insert(root, "harsha");
+    for(int i = 0; i < 1; i++) {
+//        insert(root, "abcd", 0);
+        insert(root, "vishal", 100);
+//        insert(root, "vivek", 2);
+//        insert(root, "harsha", 3);
     }
 
-    printf("contains: %s\n", contains("visbal", root)? "true": "false");
-    printf("contains: %s\n", contains("vishal", root)? "true": "false");
-    printf("contains: %s\n", contains("vi", root)? "true": "false");
-    printf("contains: %s\n", contains("visha", root)? "true": "false");
+    printf("contains: %s | key : %s | value: %d\n", contains("vishal", root)? "true": "false", "vishal", trie_search(root, "vishal"));
+//    printf("contains: %s | key : %s | value: %d\n", contains("vishal", root)? "true": "false", "vishal", trie_search(root, "vishal"));
+//    printf("contains: %s | key : %s | value: %d\n", contains("vi", root)? "true": "false", "vi", trie_search(root, "vi"));
+//    printf("contains: %s | key : %s | value: %d\n", contains("visha", root)? "true": "false", "visha", trie_search(root, "visha"));
 
     free_trie(root);
 
